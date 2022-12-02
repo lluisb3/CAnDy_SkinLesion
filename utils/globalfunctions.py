75ec83bb8914b141b18299d066695dbccb388675
sofileon <104622642+sofileon@users.noreply.github.com>
2022-12-02 12:36:52 +0100

    Global functions creation with csv_writer

---
+++ utils/globalfunctions.py
@@ -0,0 +1,20 @@
+import csv
+from pathlib import Path
+
+
+def csv_writer(filename, action, row):
+    """
+    :param filename: string
+    csv file name with its path
+    :param action: char
+     Either 'w' to write a new csv file or 'a' to append a new row
+    :param row: list
+     Data to be appended to new row
+
+    :return:
+    """
+    filename = Path(filename)
+    with open(filename, action, encoding='UTF8', newline='') as f:  # 'a' to append row
+        writer = csv.writer(f)
+        writer.writerow(row)
+        f.close()
