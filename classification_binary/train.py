4b378fb2a4cd337407abe43e37f9848f2df49f0a
Lluis <lluisb3@gmail.com>
2022-12-03 18:46:22 +0100

    add config.yml to train.py binary modify getignore dro config

---
+++ classification_binary/train.py
@@ -0,0 +1,16 @@
+import torch
+import yaml
+from pathlib import Path
+
+def train():
+    thispath = Path(__file__).resolve()
+
+    # read the configuration file
+    config_path = str(thispath.parent.parent / 'classification_binary/config.yml')
+    print(config_path)
+    with open(config_path, "r") as ymlfile:
+        cfg = yaml.safe_load(ymlfile)
+
+    # use the configuration for the dataset
+    dataset_arguments = cfg['dataset']
+    print(dataset_arguments)
